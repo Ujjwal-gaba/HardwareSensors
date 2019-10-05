086064c26939fc45c1f7e78d9e3c4111fed3de00
Ujjwal-gaba <ujjwalgaba1999@gmail.com>
2019-10-05 22:17:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.hardwaresensors
+
+import android.content.Context
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sm=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensors=sm.getSensorList(Sensor.TYPE_ALL)
+
+        sensors.forEach {
+            Log.d(
+                "Sensor", """
+            ${it.name}
+            ${it.vendor}
+            """.trimIndent()
+            )
+
+        }
+        val accelSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        val sensorListener  = object: SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            }
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d("SENSOR", """
+                    values :
+                    ax: ${event?.values?.get(0)}
+                    ay: ${event?.values?.get(1)}
+                    az: ${event?.values?.get(2)}
+                """.trimIndent())
+                val ax=event?.values?.get(0)?:0f
+                val ay=event?.values?.get(1)?:0f
+                val az=event?.values?.get(2)?:0f
+                ctBackground.setBackgroundColor(
+                    Color.rgb(
+                        (((ax+12)/24)*255).toInt(),
+                            (((ay+12)/24)*255).toInt(),
+                            (((az+12)/24)*255).toInt()
+
+                    )
+                )
+
+
+            }
+
+        }
+
+        sm.registerListener(
+            sensorListener,
+            accelSensor,
+            1000 * 60
+        )
+
+    }
+}
